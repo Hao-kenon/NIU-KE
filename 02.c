e06c226b5a19e8fa4b96862251a8b24bfecd2b96
Hao Kong <2848914089@qq.com>
2023-05-05 19:29:42 +0800

    BC85

---
+++ 02.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "02.h"
+int main()
+{
+	int count = 0;
+	int i;
+	while(I>=Y)
+	{
+		for (i = Y; i <= I; i++)
+		{
+			int t = i;
+			while (t > 0)
+			{
+				if (t % 10 == X)
+				{
+					count++;
+					break;
+				}
+				t /= 10;
+			}
+		}
+	}
+	printf("%d-%d中一共包含数字%d的个数是%d\n",Y,I,X ,count);
+	return 0;
+}
---
+++ 02.h
@@ -0,0 +1,4 @@
+#define I 2023
+#define X 3
+#define Y 1
+#include <stdio.h>

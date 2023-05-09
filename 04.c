5c2b9cc766a1378559c987b0a452a945b138ba9a
Hao Kong <2848914089@qq.com>
2023-05-09 23:07:15 +0800

    BC89-BC90

---
+++ 04.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	char password1[20] = { 0 };
+//	char password2[20] = { 0 };
+//	scanf("%s %s", &password1, &password2);
+//	if (strcmp(password1, password2))
+//	{
+//		printf("different\n");
+//	}
+//	else
+//	{
+//		printf("same\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, j, n, m;
+//	int sum = 0;
+//	int a[10][10] = { 0 };
+//	scanf("%d %d", &n, &m);
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &a[i][j]);
+//			if (a[i][j] > 0)
+//			{
+//				sum += a[i][j];
+//			}
+//		}
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
